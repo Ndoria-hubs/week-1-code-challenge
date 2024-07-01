d8697eee9d6b1f278a93d9cd8822ba80f0203eba
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-01 23:15:49 +0300

    commit changes

--- Student-grade-gen.js
+++ Student-grade-gen.js
@@ -1,12 +1,17 @@
 //Grading system
 // let marks = input(`Enter the marks you scored : `);
-let mark = prompt(`Enter marks for student :`);
-            mark = parseFloat(mark);
 
+// const ps = require("prompt-sync");
+// const prompt = ps({sigint : true}) sigint = signal interrupt
+const prompt = require("prompt-sync")();
+
+let marks = prompt("Enter marks for student : ");
+marks !== 0;
 // | Grading boundaries
+
 function grade(marks) {if ( marks <= 100 && marks > 79) {
     console.log('A')
-} if (marks <80 && marks > 59){
+} else if (marks < 80 && marks > 59){
     console.log('B')
 }else if (marks >48 && marks <60){
     console.log('C')    
@@ -14,8 +19,10 @@ function grade(marks) {if ( marks <= 100 && marks > 79) {
     console.log('D')
 } else if (marks > 0 && marks <41){
     console.log('E')
-}else {
+} else {
     console.log(`Please input valid marks!`)
 }}
-grade(70)
+
+grade(marks);
+
 
